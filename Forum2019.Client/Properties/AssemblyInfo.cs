c141452f6618a1e76a5200ccf218065134a0d6d3
roman volyk <rovol@DESKTOP-358L1FL>
2019-08-16 13:44:09 +0300

    Refactor Names

--- Forum2019.Client/Properties/AssemblyInfo.cs
+++ Forum2019.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ForumEpam2019.Client")]
+[assembly: AssemblyTitle("Forum2019.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ForumEpam2019.Client")]
+[assembly: AssemblyProduct("Forum2019.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
